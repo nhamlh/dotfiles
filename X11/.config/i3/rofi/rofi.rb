d120f729c2f6ceec13203986e0b2610f3ef55e98
Nham Le <lehoainham@mail.com>
2019-11-25 21:23:38 +0700

    Update rofi config

--- X11/.config/i3/rofi/rofi.rb
+++ X11/.config/i3/rofi/rofi.rb
@@ -1,19 +1,45 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+Dir.chdir(File.dirname(__FILE__))
+
+require 'bundler'
+
+Bundler.require
+
+require 'octokit'
+require 'lightly'
 
 def show_help
-  puts <<-EOS
-Usage: rofi.rb <action>"
+  puts <<~EOS
+    Usage: rofi.rb <action>"
 
-Valid actions:
-- github
+    Valid actions:
+    - github
   EOS
 
   exit 1
 end
 
 def run_rofi(items)
-  item = `echo -e "#{items.map(&:full_name).join("\n")}" | rofi -dmenu`
+  item = `echo -e "#{items.join("\n")}" | rofi -i -dmenu`
   yield item unless item.empty?
+end
+
+def get_github_repos
+  # Get repos list from github, then generate circleci URL
+
+  token = ENV['ROFI_GITHUB_TOKEN']
+
+  client = Octokit::Client.new(access_token: token, auto_paginate: true)
+
+  cache = Lightly.new life: '168h'
+
+  repos = cache.get 'github_repos' do
+    client.repositories
+  end
+
+  repos.map(&:full_name)
 end
 
 ########################
@@ -23,39 +49,16 @@ show_help if ARGV.length != 1
 
 case ARGV[0]
 when 'github'
-  require 'octokit'
-  require 'lightly'
-
-  TOKEN = ENV['ROFI_GITHUB_TOKEN']
-
-  client = Octokit::Client.new(access_token: TOKEN, auto_paginate: true)
-
-  cache = Lightly.new life: '168h'
-
-  repos = cache.get 'github_repos' do
-    client.repositories
-  end
-
-  run_rofi(repos) do |item|
+  run_rofi(get_github_repos) do |item|
     `firefox --new-tab https://github.com/#{item}`
   end
 when 'circleci'
-  # Get repos list from github, then generate circleci URL
-  require 'octokit'
-  require 'lightly'
-
-  TOKEN = ENV['ROFI_GITHUB_TOKEN']
-
-  client = Octokit::Client.new(access_token: TOKEN, auto_paginate: true)
-
-  cache = Lightly.new life: '168h'
-
-  repos = cache.get 'github_repos' do
-    client.repositories
-  end
-
-  run_rofi(repos) do |item|
+  run_rofi(get_github_repos) do |item|
     `firefox --new-tab https://circleci.com/gh/#{item}`
+  end
+when 'hostname'
+  run_rofi(get_eh_hostname) do |item|
+    `firefox --new-tab #{item}`
   end
 else
   show_help
