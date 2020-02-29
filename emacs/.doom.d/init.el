4bf046aa9fe7a1241af6bd005a5d799f8c1e6e78
Nham Le <lehoainham@gmail.com>
2020-02-29 15:07:09 +0700

    emacs: disable objed because it causes errors

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -34,7 +34,7 @@
        (format +onsave)  ; automated prettiness
        lispy             ; vim for lisp, for people who don't like vim
        multiple-cursors  ; editing in many places at once
-       objed             ; text object editing for the innocent
+       ;;objed           ; text object editing for the innocent
        rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        word-wrap         ; soft wrapping with language-aware indent
