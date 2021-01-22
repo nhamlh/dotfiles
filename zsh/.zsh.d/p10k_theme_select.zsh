6e3cc59424e5988ee04fcdd41dd14ce7514bc5bd
Nham Le <lehoainham@gmail.com>
2021-01-22 11:45:04 +0700

    zsh: customize p10k theme suitable for remote shell

---
+++ zsh/.zsh.d/p10k_theme_select.zsh
@@ -0,0 +1,5 @@
+if [ -z "$SSH_TTY" ]; then
+  source ~/.zsh.d/p10k_themes/powerline.zsh
+else
+  source ~/.zsh.d/p10k_themes/pure.zsh
+fi
--- zsh/.zsh.d/p10k_themes/powerline.zsh
+++ zsh/.zsh.d/p10k_themes/powerline.zsh
@@ -1143,7 +1143,7 @@
   #############[ kubecontext: current kubernetes context (https://kubernetes.io/) ]#############
   # Show kubecontext only when the the command you are typing invokes one of these tools.
   # Tip: Remove the next line to always show kubecontext.
-  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx|oc'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx|oc|hero2,kops'
 
   # Kubernetes context classes for the purpose of using different colors, icons and expansions with
   # different contexts.
